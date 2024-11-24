41f08231604f8aec751c3c589cf1671ad51f42e4
Ivan Gruber <igruber@santandertecnologia.com.ar>
2024-11-24 01:37:35 -0300

    se agrega ejercicio 12

--- src/Ejercicio_12/uso/Main.java
+++ src/Ejercicio_12/uso/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         ABBTDA abb = new ABB();
+        abb.inicializarArbol();
         abb.agregarElem(10);
         abb.agregarElem(5);
         abb.agregarElem(3);
@@ -23,10 +24,13 @@ public class Main {
         if (abb.arbolVacio()) {
             return 0;
         } else {
+            // Suma los valores impares del subárbol
             int suma = sumaImpares(abb.hijoIzq());
+            //si la raiz es impar, se suma
             if (abb.raiz() % 2 != 0) {
                 suma += abb.raiz();
             }
+            // Se suma los valores impares del subárbol derecho
             suma += sumaImpares(abb.hijoDer());
             return suma;
         }
